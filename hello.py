ff61293403d463e16716cd999bbce1747c57a64d
Logan Zamora <loganazamora@gmail.com>
2026-06-13 17:04:11 +0200

    Deleted duplicate files

--- hello.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your name? ")
-age = int(input("How old are you? "))
-height = float(input("How tall are you? "))
-
-
-
-print(name, "is", age, "years old and", height, "centimeters tall")
--- info.formatter.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name? ").strip().title()
-favColor = input("What's your favorite color? ").lower()
-favNumber = int(input("What's your favorite number? "))
-birthYear = int(input("When were you born? "))
-
-print(f"Hi {name}! Your favorite color is {favColor} and your favorite number is {favNumber}. In 2025 you are {2025 - birthYear}!")
