26f76190336cff73da9ccb3e65a4bfd7034e7b60
Logan Zamora <loganazamora@gmail.com>
2026-06-15 10:15:24 +0200

    Started patient registry program. Notes on what the program does are at the top of the program. This is being done to train my skills in dictionaries prior to implementing it into to-do-3.

---
+++ python/Learning for comp. sci./ATBSWP/patientregistry.py
@@ -0,0 +1,26 @@
+# manages a simple patient registry as a dictionary. 
+# The keys are patient names, the values 
+# are dictionaries containing their age and diagnosis.
+
+patients = {
+    "Erik Andersson": {"age": 45, "diagnosis": "Parkinson's"},
+    "Maria Lindqvist": {"age": 45, "diagnosis": "Alzheimer's"}
+}
+
+def main_screen():
+    print("Welcome to your patient registry program! ")
+    print("[1] View details of a patient")
+    print("[2] Add patient information to database")
+    print("[3] View information of all patients")
+    print("[4] Leave patient registry program")
+
+    
+
+def details_of_patients():
+    input_prompt = "What "
+
+def add_patient():
+
+
+def view_all_patients():
+
