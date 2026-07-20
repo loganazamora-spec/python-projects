3e424268fe0949568a2d264c30a38fa8056e8037
Logan Zamora <loganazamora@gmail.com>
2026-07-20 22:22:09 +0200

    Edited main screen to run under while loop and added a quit option to exit the program.

--- python/Learning for comp. sci./ATBSWP/patientregistry.py
+++ python/Learning for comp. sci./ATBSWP/patientregistry.py
@@ -6,21 +6,32 @@ patients = {
     "Erik Andersson": {"age": 45, "diagnosis": "Parkinson's"},
     "Maria Lindqvist": {"age": 45, "diagnosis": "Alzheimer's"}
 }
-
-def main_screen():
-    print("Welcome to your patient registry program! ")
-    print("[1] View details of a patient")
-    print("[2] Add patient information to database")
-    print("[3] View information of all patients")
-    print("[4] Leave patient registry program")
-
     
 
 def details_of_patients():
-    input_prompt = "What "
+    input_prompt = "Please enter the name of the patient: "
 
 def add_patient():
 
 
 def view_all_patients():
 
+while True:
+    print("Welcome to your patient registry program! ")
+    print("[1] View details of a patient")
+    print("[2] Add patient information to database")
+    print("[3] View information of all patients")
+    print("[4] Leave patient registry program")
+
+    user_response = input("Please enter your choice: ").lower().strip()
+
+    if user_response in ["1", "View"]:
+        details_of_patients()
+    elif user_response in ["2", "Add"]:
+        add_patient()
+    elif user_response in ["3", "View All"]:
+        view_all_patients()
+    elif user_response in ["4", "Leave"]:
+        print("Thank you for using the patient registry program. Goodbye!")
+        break  
+    
